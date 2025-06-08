5e4e0f04d3cdad6b1c7ebe69786bb3637dd01a82
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-06-08 22:10:24 +0300

    Added ability for subpath

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
-            "~components": path.resolve(__dirname, "./src/components")
-        }
-    }
+            "~components": path.resolve(__dirname, "./src/components"),
+        },
+    },
+    base: process.env.VITE_BASE_URL || "/",
 });
