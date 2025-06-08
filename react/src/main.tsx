5c8a20b308ed7fd5deefa4fbc5763d445b77a7f4
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-06-08 21:08:51 +0300

    Production docker / minor improvements

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,9 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./global.css";
+import { Helmet } from "react-helmet";
 
 createRoot(document.getElementById("root")!).render(
     <>
+        <Helmet>
+            <title>Putty to Open SSH Converter</title>
+        </Helmet>
         <App />
     </>
 );
